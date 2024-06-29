cc4ffa17f693a468efe106d9257e4c4ab0ca54fd
Charlotte Leong <charlotte.leong@kcl.ac.uk>
2024-06-29 10:26:04 +0100

    add default css and import in icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -32,7 +32,7 @@ export const NavBar = () => {
     <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
-            <img src={'logo'} alt="Logo" />
+            <img src={logo} alt="Logo" />
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav">
